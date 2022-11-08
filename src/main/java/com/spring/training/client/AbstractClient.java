91a18e937b2e4b9190dcb7d23d602525a13e6661
mamadou lamine ba <laminba2003@gmail.com>
2022-11-08 15:30:19 +0000

    improvements

--- src/main/java/com/spring/training/client/AbstractClient.java
+++ src/main/java/com/spring/training/client/AbstractClient.java
@@ -4,7 +4,7 @@ import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public abstract class AbstractClient extends WebServiceGatewaySupport {
 
-    public <T, U> U sendRequest (T request, Class<U> clazz) {
+    public <T, U> U sendRequest(T request, Class<U> clazz) {
         return clazz.cast(getWebServiceTemplate().marshalSendAndReceive(request));
     }
 
